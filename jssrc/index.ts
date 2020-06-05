bb0c7781ff60b68b7aa118ff10e887d42a42c990
Hyun Woo Park <phu54321@naver.com>
2020-06-05 21:25:07 +0900

    :bug: prevent window opening multiple times
    d/t multiple event handler registration

--- jssrc/index.ts
+++ jssrc/index.ts
@@ -1,11 +1,17 @@
 import { callPyFunc } from './utils/pyfunc'
 
-window.addEventListener('click', function (evt) {
-  if (evt.detail === 3) {
-    const el = evt.target as HTMLElement
-    if (el.nodeName === 'IMG') {
-      const src = (el as HTMLImageElement).src
-      callPyFunc('imgsplit_onTripleClick', src)
+const singletonKey = '__imgsplit__inited'
+
+if (!(window as any)[singletonKey]) {
+  (window as any)[singletonKey] = true
+
+  window.addEventListener('click', function (evt) {
+    if (evt.detail === 3) {
+      const el = evt.target as HTMLElement
+      if (el.nodeName === 'IMG') {
+        const src = (el as HTMLImageElement).src
+        callPyFunc('imgsplit_onTripleClick', src)
+      }
     }
-  }
-})
+  })
+}
